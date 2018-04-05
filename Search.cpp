ae471983b298e1095dbfb3d48b2ad4ba2c2f0f86
ShvarcbergJull <32794912+ShvarcbergJull@users.noreply.github.com>
2018-04-05 16:36:47 +0800

    Create Search.cpp

---
+++ Search.cpp
@@ -0,0 +1,55 @@
+#include "Search.h"
+#include "geometry.h"
+
+using namespace std;
+
+void clearPoint(Point p)
+{
+	p.x = p.y = 0.0;
+}
+
+void clearTriangle(Triangle tr)
+{
+	for (int i = 0; i < 3; i++)
+		clearPoint(tr.vertexes[i]);
+	tr.area = 0.0;
+}
+
+void findAndInsert(Triangle* trArray, int lenght, const Triangle triangle)
+{
+	int pos;
+	for (pos = lenght - 1; pos >= 0; pos--)
+	{
+		if (triangle.area <= trArray[pos].area)
+			break;
+	}
+	pos++;
+	if (pos == lenght)
+		return;
+	for (int m = lenght - 2; m >= pos; m--)
+		trArray[m + 1] = trArray[m];
+	trArray[pos].area = triangle.area;
+}
+
+void searchLargestTriangles(const Point* pointArray, int pointNum, Triangle* trArray, int maxTrNum)
+{
+	if (maxTrNum <= 0)
+		return;
+	for (int i = 0; i < maxTrNum; i++)
+		clearTriangle(trArray[i]);
+	Triangle tr;
+	for (int i = 0; i < pointNum; i++)
+	{
+		tr.vertexes[0] = pointArray[i];
+		for (int j = i + 1; j < pointNum; j++)
+		{
+			tr.vertexes[1] = pointArray[j];
+			for (int k = j + 1; k < pointNum; k++)
+			{
+				tr.vertexes[2] = pointArray[k];
+				calcTringleArea(tr);
+				findAndInsert(trArray, maxTrNum, tr);
+			}
+		}
+	}
+}
